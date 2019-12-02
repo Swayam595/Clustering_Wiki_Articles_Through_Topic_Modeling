8c2f23c8362d174e6adb67df5429e30fb76d82ae
ArunKarthik2197 <arun.munipalle1997@gmail.com>
2019-12-02 05:15:17 -0800

    script for combining csvs and updated pic_clustering

---
+++ src/file_concat.sh
@@ -0,0 +1,7 @@
+d=$(pwd)
+n=${d:37}
+echo "$n"
+
+cat part*.csv > combined_data.csv
+awk 'BEGIN{f=""}{if($0!=f){print $0}if(NR==1){f=$0}}' combined_data.csv > final_100-"$n"_clusters.csv
+ls | grep final
--- src/pic_clustering.py
+++ src/pic_clustering.py
@@ -8,39 +8,57 @@ import sys
 from pyspark import SparkConf, SparkContext, SQLContext
 from pyspark.mllib.clustering import PowerIterationClustering, PowerIterationClusteringModel
 import os
+import traceback
+
+# initializing spark
 conf = SparkConf()
 sc = SparkContext.getOrCreate(conf=conf)
 sqlCon = SQLContext(sc)
-
+#directory settings
 base_folder = os.path.abspath("..")
 raw_folder = os.path.join(base_folder,"data/")
 dat_folder = os.path.join(base_folder,"src/")
 preprocessed_folder = os.path.join(base_folder,"preprocessed/")
 
-data = sc.textFile(dat_folder+'sim_pic_data.csv')
+print("Starting to read the file...")
+# reading from the graph_sims file
+data = sc.textFile(raw_folder+'adjacency_graph_final_data.csv')
 header = data.first()
 
+print("File Read...\n")
+#mapper to convert data into required form RDD(long,long,double)
 def initialProcess(lines):
     x= lines.split(',')
     x = (int(x[0]),int(x[1]),float(x[2]))
     return x
 
+#filtering out the header
 sims=data.filter(lambda x:  x!=header).map(initialProcess)
 
-#sims.collect()
+num_iterations = [25,35,50,60]
 
+for num in num_iterations:
+    
+    try:
+        
+        # PIC(RDD,num_clusters,num_iterations) defaults: num_iterations=100
+        model = PowerIterationClustering.train(sims,100,num)
 
-model = PowerIterationClustering.train(sims,5,10)
+        print("model trained\t num_iterations:"+str(num)+"\n")
+        #Saving Clusters
+        clusters = model.assignments()
+        clustRDD = clusters.map(lambda x: (x.id,x.cluster))
+        clust_df = clustRDD.toDF()
 
-#Saving Clusters
-clusters = model.assignments()
-clustRDD = clusters.map(lambda x: (x.id,x.cluster))
-clust_df = clustRDD.toDF().toPandas()
+        clust_df.write.csv('clusters_'+str(num),header = 'true')
+        print("csv for {} iterations written".format(num))
+    except Exception:
+        log = open("exception.log","w+")
+        traceback.print_exc(file=log)
 
-#clust_df.coalesce(2).write.csv('clusters',header = 'true')
-
-clust_df = clust_df.rename(columns={'_1':'NodeID','_2':'Cluster'})
-clust_df.to_csv('clusters.csv',index=False)
+print("**************Complete****************")
+#clust_df = clust_df.rename(columns={'_1':'NodeID','_2':'Cluster'})
+#clust_df.to_csv('clusters.csv',index=False)
 
 
 
